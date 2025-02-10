b6fa829ce5a60e1ce75aa559b62afbcdc395e8b8
Abdukarim Qarshiyev <mr.abdukarim1@gmail.com>
2025-02-10 11:55:34 +0500

    added new files

---
+++ first-lesson.js
@@ -0,0 +1,34 @@
+// document.write('Hello Javascript') //
+// alert('Hello User!') // Diolog oynasi.
+// console.log('Hello Terminal') // Terminal
+
+// let greeting = "Hello"
+// let greeting_end = "World"
+// console.log(greeting + " " + greeting_end)
+// document.write(greeting + " " + greeting_end)
+
+// console.log(greeting.length) // Uzunlik o'lchash
+
+// let str = 'JS'
+// let txt = `Hello, ${str}!`
+// console.log(txt)
+
+// let text = `Lorem ipsum dolor sit amet,
+// consectetur adipisicing elit.
+// Aliquam, excepturi?`
+
+
+// int
+// str
+// True false 0 1
+// undefined
+// null
+// infinity   -infinity
+// NaN
+// defined
+
+
+// let text = null
+// text = "Hello World!";
+// console.log(text)
+
