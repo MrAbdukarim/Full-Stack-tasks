dd718c00a6ce12abeb86cf007f2646fe23a04ec3
Abdukarim Qarshiyev <mr.abdukarim1@gmail.com>
2025-02-11 10:11:36 +0500

    added new file

---
+++ second-lesson.js
@@ -0,0 +1,28 @@
+// let a = 100, b = 200
+//
+// document.write(`Uning perimeteri: ${2 * (a + b)}`)
+// document.write(`\n\nUning Yuzi: ${a * b}`)
+
+
+// let w = "200px"
+// let h = "100px"
+//
+// let perem = 2 * (parseFloat(w) + parseFloat(h))
+// let yuzi = (parseFloat(w) * parseFloat(h))
+// console.log(`${perem} ${yuzi}`)
+
+
+// let uzunlik = document.querySelector('div').style.width
+// let balandlik = document.querySelector('div').style.height
+// let perem = 2 * (parseFloat(uzunlik) + parseFloat(balandlik))
+// let yuzi = parseFloat(balandlik) * parseFloat(balandlik)
+//
+// console.log(`${balandlik} - ${yuzi}`)
+
+
+// let num = 47
+// num += 7
+// num -= 18
+// num *= 10
+// num /= 15
+// console.log(num)
