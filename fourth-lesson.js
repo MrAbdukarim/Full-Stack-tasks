5d200fa774b8cf565def3057991f79dc5439308e
Abdukarim Qarshiyev <mr.abdukarim1@gmail.com>
2025-02-18 11:11:50 +0500

    added: Several solutions have been added to fourth-lesson.js file

--- fourth-lesson.js
+++ fourth-lesson.js
@@ -1,15 +1,25 @@
-let number = +prompt('1-12 raqam kiriting: ');
+// let number = +prompt('1-12 raqam kiriting: ');
+//
+// if (number >= 1 && number <= 12) {
+//     if (number === 12 || number === 1 || number === 2) {
+//         alert('Bu oy qish faslida');
+//     } else if (number >= 3 && number <= 5) {
+//         alert('Bu oy bahor faslida');
+//     } else if (number >= 6 && number <= 8) {
+//         alert('Bu oy yoz faslida');
+//     } else if (number >= 9 && number <= 11) {
+//         alert('Bu oy kuz faslida');
+//     }
+// } else {
+//     alert('1dan 12gacha raqam kiritish shart!');
+// }
 
-if (number >= 1 && number <= 12) {
-    if (number === 12 || number === 1 || number === 2) {
-        alert('Bu oy qish faslida');
-    } else if (number >= 3 && number <= 5) {
-        alert('Bu oy bahor faslida');
-    } else if (number >= 6 && number <= 8) {
-        alert('Bu oy yoz faslida');
-    } else if (number >= 9 && number <= 11) {
-        alert('Bu oy kuz faslida');
-    }
-} else {
-    alert('1dan 12gacha raqam kiritish shart!');
-}
+
+// let user = ["Anna", "Tommy", "George", "John"]
+//
+// for (let elem of user) {
+//     if (elem === "Anna") document.write(`<b>${elem}</b> <br>`)
+//     else if (elem === "Tommy") document.write(`<i>${elem}</i> <br>`)
+//     else if (elem === "George") document.write(`<u>${elem}</u> <br>`)
+//     else document.write(`${elem}`)
+// }
